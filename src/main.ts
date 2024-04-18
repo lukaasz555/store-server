36a56b0e45b7feec656b99a24c19e9a38c4f114a
lukaasz555 <lukasz.zegar555@gmail.com>
2024-04-18 18:30:20 +0200

    main + config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import * as process from 'process';
 
-async function bootstrap() {
+async function bootstrap(): Promise<void> {
+  const PORT = process.env.PORT || 4000;
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors();
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  ),
+    await app.listen(PORT);
+  console.log('App is running on port ', PORT);
 }
 bootstrap();

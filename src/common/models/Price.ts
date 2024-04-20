ac1de4ded4ebbe903dc7ad50ccb1e5673d39a997
lukaasz555 <lukasz.zegar555@gmail.com>
2024-04-20 23:07:19 +0200

    swagger schemas

--- src/common/models/Price.ts
+++ src/common/models/Price.ts
@@ -1,9 +1,18 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { CurrencyEnum } from '../enums/Currency.enum';
 
 export abstract class Price {
+  @ApiProperty()
   currency: CurrencyEnum;
+
+  @ApiProperty()
   value: number;
+
+  @ApiProperty()
   startDate: Date;
+
+  @ApiProperty()
   endDate: Date | null;
+  @ApiProperty()
   isCurrent: boolean;
 }
--- src/common/models/PurchasePrice.ts
+++ src/common/models/PurchasePrice.ts
@@ -1,6 +1,9 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { CurrencyEnum } from '../enums/Currency.enum';
 
 export class PurchasePrice {
+  @ApiProperty()
   currency: CurrencyEnum = CurrencyEnum.PLN;
+  @ApiProperty()
   value: number;
 }
